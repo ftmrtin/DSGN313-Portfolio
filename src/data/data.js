d0587817eb18b6145ccc5109948f59a61d123f1a
Faith <ftmrtin27@gmail.com>
2025-01-21 10:52:11 -0700

    RAHHHHH

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@
 
 const data = {
     name: "Faith Gabrielle",
-    bio: "Design student at the Univerisity of Calgary",
+    bio: "Design student at the Univerisity of Calgary. Workshop Committee Student Representative '24/25.",
     contact: {
         email: "ftmrtin27@gmail.com",
         phone: "+4039296727",
--- src/main.js
+++ src/main.js
@@ -26,58 +26,52 @@ document.querySelector("#app").innerHTML = `
     <section id="projects">
       <div id="project-row">
       DSGN 313: Cadavre Exquis
-
         <div class="three-model">
-           <a href="CADAVRE-EXQUIS.jpg" target="_blank">
-             <img src="CADAVRE-EXQUIS.jpg" id="model1" alt="Model1 Image" />
-           </a>
-
+          <a href="CADAVRE-EXQUIS.jpg" target="_blank">
+            <img src="CADAVRE-EXQUIS.jpg" id="model1" alt="Cadavre" />
+          </a>
         </div>
+
         <div id="images-description">
           <div id="images">
            <a href="cubeDrawings/cube1.park.jpg" target="_blank">
-              <img src="cubeDrawings/cube1.park.jpg" alt="img"/>
-           </a>
-           <a href="cubeDrawings/cube2.flower.jpg" target="_blank">
-              <img src="cubeDrawings/cube2.flower.jpg" alt="img"/>
-           </a>
-           <a href="cubeDrawings/cube3.hearse.jpg" target="_blank">
-              <img src="cubeDrawings/cube3.hearse.jpg" alt="img"/>
-           </a>
-           <a href="cubeDrawings/cube4.nyc.jpg" target="_blank">
-              <img src="./cubeDrawings/cube4.nyc.jpg" alt="img"/>
-           </a>
-         </div>
-      
-         </div>
-          <h4 id="description">The Warriors: inspired by the 1979 film. This blends the gritty, action-packed narrative with theatrical grandeur.
-          
-          Inspired by the 1979 cult classic The Warriors and the soundscape of the 2024 album Warriors, this Cadavre Exquis reimagines the narrative as a dark and electrifying stage musical.
-First and foremost, I am a theatre kid. From the moment I listened to the album, I was struck by how vividly the visuals of a potential play came to life in my mind. The soundtrack is so compelling that it feels destined to be physically realized on stage. A well-crafted album has the power to evoke imagery, and this one heightens the experience of every moment, especially fight scenes. Even without explicit descriptions, the instrumentals guide your imagination- each step, each punch, each impact resonated through the music.
-While I have never worked as part of a technical crew for a play, this album ignited a desire in me to stage a production. It’s a testament to the album’s immersive quality and its potential to inspire something theatrical.</h4>
+             <img src="cubeDrawings/cube1.park.jpg" alt="cube1" />
+            </a>
+            <a href="cubeDrawings/cube2.flower.jpg" target="_blank">
+              <img src="cubeDrawings/cube2.flower.jpg" alt="cube2" />
+            </a>
+            <a href="cubeDrawings/cube3.hearse.jpg" target="_blank">
+              <img src="cubeDrawings/cube3.hearse.jpg" alt="cube3" />
+            </a>
+            <a href="cubeDrawings/cube4.nyc.jpg" target="_blank">
+              <img src="cubeDrawings/cube4.nyc.jpg" alt="cube4" />
+            </a>
+          </div>
+          <h4 id="description">Inspired by the 1979 cult classic The Warriors and the soundscape of the 2024 album Warriors, this Cadavre Exquis reimagines the narrative as a dark and electrifying stage musical. First and foremost, I am a theatre kid. From the moment I listened to the album, I was struck by how vividly the visuals of a potential play came to life in my mind. The soundtrack is so compelling that it feels destined to be physically realized on stage. A well-crafted album has the power to evoke imagery, and this one heightens the experience of every moment, especially fight scenes. Even without explicit descriptions, the instrumentals guide your imagination- each step, each punch, each impact resonated through the music. While I have never worked as part of a technical crew for a play, this album ignited a desire in me to stage a production. It’s a testament to the album’s immersive quality and its potential to inspire something theatrical.</h4> 
         </div>
       </div>
 
-${/* Model 2 commented out}
-      <div id="project-row">
-      Model 2
-        <div class="three-model">
-          <div id="model2"></div>
-        </div>
-        <div id="images-description">
-          <div id="images">
-            ${trees
-              .map(
-                (trees, index) =>
-                  `<img src="${trees}" alt="tree${index + 1}" />`
-              )
-              .join("")}
+      ${/* Model 2 commented out}
+        <div id="project-row">
+        Model 2
+          <div class="three-model">
+            <div id="model2"></div>
           </div>
-          <h4 id="description">A 3D tree model is a geometric representation of a tree, featuring a trunk, branches, and foliage. The trunk is typically cylindrical, with textured bark, while the branches extend outward to support leaves or a canopy. It may include roots or flowers for added detail.</h4>
+          <div id="images-description">
+            <div id="images">
+              ${trees
+                .map(
+                  (trees, index) =>
+                    `<img src="${trees}" alt="tree${index + 1}" />`
+                )
+                .join("")}
+            </div>
+            <h4 id="description">A 3D tree model is a geometric representation of a tree, featuring a trunk, branches, and foliage. The trunk is typically cylindrical, with textured bark, while the branches extend outward to support leaves or a canopy. It may include roots or flowers for added detail.</h4>
+          </div>
         </div>
-      </div>
-      */""}
-${/* Model 3 commented out}
+        */""}
+
+      ${/* Model 3 commented out}
       <div id="project-row">
       Model 3
         <div class="three-model">
@@ -95,6 +89,7 @@ ${/* Model 3 commented out}
         </div>
       </div>
       */""}
+
     </section>
 
     <ul id="footer-items">
@@ -111,6 +106,6 @@ ${/* Model 3 commented out}
 `;
 
 // Create three.js scenes for each
-// createThreeScene("#model1", "/3DModels/project1/cube.obj");
-// createThreeScene("#model2", "/3DModels/project2/tree.obj");
-// createThreeScene("#model3", "/3DModels/project3/cottage.obj");
+createThreeScene("#model1", "/3DModels/project1/cube.obj");
+createThreeScene("#model2", "/3DModels/project2/tree.obj");
+createThreeScene("#model3", "/3DModels/project3/cottage.obj");
